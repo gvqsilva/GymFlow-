8997db1c2a901c9e9b32c6ddada9a63df64c6de5
Gabriel Vasquez <110639916+gvqsilva@users.noreply.github.com>
2026-04-27 17:16:16 -0300

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,14 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Substitua pela configuração do seu projeto Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCaMtekCg9bZXpZ7fT9GmqCVutW5MLpd9A",
-  authDomain: "gymflow-prod-57ee8.firebaseapp.com",
-  projectId: "gymflow-prod-57ee8",
-  storageBucket: "gymflow-prod-57ee8.firebasestorage.app",
-  messagingSenderId: "231550631555",
-  appId: "1:231550631555:web:001fd43d35a9f4e3cf65b6",
-  measurementId: "G-DM1DXVGHGS"
+
 };
 
 // Verificar se o Firebase já foi inicializado
